50cc4d0f0e5444d4de54da3dc3cd51f0ec736ed8
Adlls <adlansaidullaev@gmail.com>
2020-09-04 12:58:28 +0300

    fix bug in heavy strategy

--- src/strategyAI/heavyStrategy.ts
+++ src/strategyAI/heavyStrategy.ts
@@ -5,13 +5,8 @@ import {GameRules, paramsGrid} from "../gameRules";
 export class HeavyStrategy implements IStrategyAI {
 
 
-    private iSetHiderPlayerPosition: boolean = false;
-
     //мешаем игроку
     hinderPlayer(shapeBounds: Array<number>, cells: Array<Cell>, indexIteration: number): XYData {
-
-
-        console.log("hinder player");
 
 
         let indexRow = 0;
@@ -33,7 +28,7 @@ export class HeavyStrategy implements IStrategyAI {
                         cells[j - indexIteration].isCross &&
                         cells[j - indexIteration * 2].isCross) {
 
-                        if (!cells[j - 3].isCross && !cells[j - 3].isCircle) {
+                        if (!cells[j - indexIteration * 3].isCross && !cells[j - indexIteration * 3].isCircle) {
                             return new XYData(
                                 cells[j - indexIteration * 3].getCellX + 10,
                                 cells[j - indexIteration * 3].getCellY + 10);
@@ -49,7 +44,6 @@ export class HeavyStrategy implements IStrategyAI {
     //ищем решение для AI
     lookSolution(shapeBounds: Array<number> , cells: Array<Cell>, indexIteration: number): XYData {
 
-        console.log("looks solution");
 
         for (let j = shapeBounds[0]; j < shapeBounds[1]; j = j + indexIteration) {
                 if (cells[j].isCircle) {
@@ -90,12 +84,10 @@ export class HeavyStrategy implements IStrategyAI {
              xyRows = this.hinderPlayer(rows[i], cells, 1);
              xyCols = this.hinderPlayer(cols[i], cells, GameRules.currentCountRectPerimeter);
              if (xyRows) {
-                 console.log(xyRows);
                  return xyRows;
              }
 
              if (xyCols) {
-                 console.log(xyCols);
                  return xyCols;
              }
         }
@@ -104,12 +96,10 @@ export class HeavyStrategy implements IStrategyAI {
             xyRows = this.lookSolution(rows[j], cells, 1);
             xyCols = this.lookSolution(cols[j], cells, GameRules.currentCountRectPerimeter);
             if (xyRows) {
-                console.log(xyRows);
                 return xyRows;
             }
 
             if (xyCols) {
-                console.log(xyCols);
                 return xyCols;
             }
         }
@@ -118,7 +108,6 @@ export class HeavyStrategy implements IStrategyAI {
             cells[cells.length - 1].getCellX + paramsCell.sizeCell);
         let randomY: number = Phaser.Math.Between(0,
             cells[cells.length - 1].getCellY + paramsCell.sizeCell);
-        console.log(new XYData(randomX, randomY), "random");
         return new XYData(randomX, randomY);
 
     }
